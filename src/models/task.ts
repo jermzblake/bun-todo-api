f7f47e2b3a795ff8e66ce3ae6eb280c781f3fd03
jermzblake <jermzblake@gmail.com>
2024-02-09 08:39:36 -0500

    pretty code

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,26 +1,26 @@
-import * as mongoose from "mongoose"
+import * as mongoose from 'mongoose'
 
 const taskSchema = new mongoose.Schema({
-  _id: {type: mongoose.Schema.Types.ObjectId, auto: true},
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   title: String,
   description: { type: String, required: true },
   priority: {
     type: Number,
     min: 1,
     max: 3,
-    required: true
+    required: true,
   },
   effort: {
     type: Number,
     min: 1,
     max: 5,
-    required: true
+    required: true,
   },
   dueDate: Date,
   completed: { type: Boolean, default: false },
   notes: String,
   blocked: { type: Boolean, default: false },
-  created: { type: Date, default: Date.now }
+  created: { type: Date, default: Date.now },
 })
 
 export type Task = mongoose.InferSchemaType<typeof taskSchema>
